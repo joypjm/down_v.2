56a3708fc6139403224327687c879df6fc77747d
esteban031 <147660928+esteban031@users.noreply.github.com>
2023-10-23 19:02:34 -0500

    Update operaciones.py

--- operaciones.py
+++ operaciones.py
@@ -3,3 +3,23 @@ def add (num_1, num_2):
     print (f' {num_1} + {num_2} is equal to {result}')
     return result
   
+#operacion con producto 
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+resultado = numero1 * numero2
+print(f"El resultado de la multiplicación es: {resultado}")
+
+#operaciones con producto
+
+base = float(input("Ingresa la base: "))
+exponente = int(input("Ingresa el exponente: "))
+resultado = base ** exponente
+print(f"{base} elevado a la {exponente} es igual a: {resultado}")
+
+#operaciones con modulo 
+dividendo = int(input("Ingresa el dividendo: "))
+divisor = int(input("Ingresa el divisor: "))
+resultado = dividendo % divisor
+print(f"El módulo de {dividendo} entre {divisor} es igual a: {resultado}")
+
+
